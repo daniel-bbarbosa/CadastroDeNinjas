8af6b489ae51b56bb27e3832ba13582706eb6771
Daniel Barbosa Bezerra <bbarbosa.daniel@gmail.com>
2026-05-24 18:15:12 -0300

    feat: criando entidade

--- src/main/java/kjnmun/dan/CadastroDeNinjas/NinjaController.java
+++ src/main/java/kjnmun/dan/CadastroDeNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "This is my first message in this root!";
